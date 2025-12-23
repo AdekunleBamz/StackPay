cb0c15e339984c974def5c58fe8f1d06abad1389
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-23 13:59:06 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { openContractCall } from "@stacks/connect";
-import { uintCV, standardPrincipalCV } from "@stacks/transactio
+import { uintCV, standardPrincipalCV } from "@stacks/transacti
 import { contractAddress,contractName } from "../lib/contract";
 
 export default function CreateStream() {
