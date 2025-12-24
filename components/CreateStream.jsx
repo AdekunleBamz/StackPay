18b1b93d43aa1a5e18fba1df50703f8fdefb65a7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 08:13:36 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -10,7 +10,7 @@ export default function CreateStream() {
       contractAddress,
       contractName,
       functionName: "create-tream"
-      functionArgs:
+      functionArgs
         standardPrincipalCV("STEMPLOYEEADDRESS"),
         uintCV(10)
         uintCV(1000000),
