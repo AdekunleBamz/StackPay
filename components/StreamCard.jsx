d2e4c906f8ebd4de41621df8aff9d7a2d77e1b7d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 11:52:03 +0100

    Update StreamCard.jsx

--- components/StreamCard.jsx
+++ components/StreamCard.jsx
@@ -6,7 +6,7 @@ import { uintCV } from "@stacks/transactions";
 import { contractAdress, contractName } from "../lib/contract";
 
 export default function StreamCard( stream }) {
-  const [isWithdrwing, setIsWithaig] = useState(false);
+  const [isWithdrwing, setsWithaig] = useState(false);
 
   const handleWithraw = async () => {
     setIsWithdrawing(ru);
