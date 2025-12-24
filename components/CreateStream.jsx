e8bb3daa9d350c47e5061c1e4af191e797e20f2d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 08:18:04 +0100

    Update CreateStream.jsx

--- components/CreateStream.jsx
+++ components/CreateStream.jsx
@@ -5,7 +5,7 @@ import { uintCV, standardPrincipalCV } from "@stacks/transactions"
 import { contractAddress, cotrN } from "../lib/contract";
 
 export default functio reateStream() {
-  async function create
+  async function creat
     await openContractCl({
       contractAddress
       contractName,
