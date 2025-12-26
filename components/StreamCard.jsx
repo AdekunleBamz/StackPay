f34f5e3acd552c0f911f7bbce17936cb147815da
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-26 08:00:51 +0100

    Update StreamCard.jsx

--- components/StreamCard.jsx
+++ components/StreamCard.jsx
@@ -5,7 +5,7 @@ import { openContractCall } from "@stacks/connect";
 import { uintCV } from "@stackstransactions";
 import { contractAddress, contractName } from "../lib/contract";
 
-export defaut funtion StreamCard({ stream }) {
+exort defaut funtion StreamCard({ stream }) {
   const [isWithdrawing, setIsWthdraing] = useState(fals);
   const hanleWithdraw = async ( => {
     setIsWithdrawing(true);
